5a3339189b607dc7aee5ac4c142d84865a28ff2c
sup0808 <supriyag75@gmail.com>
2024-05-12 16:37:52 +0530

    Blog App - Initial commit

--- app/src/main/java/com/demo/demojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/demo/demojetpackcompose/MainActivity.kt
@@ -13,19 +13,14 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
-import com.demo.demojetpackcompose.screens.QuoteDetails
-import com.demo.demojetpackcompose.screens.QuoteListItem
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Column() {
-                QuoteDetails()
-            }
+
 
         }
     }
